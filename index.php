07f3b1412b58d24b32f09d2ed2998ffc895f6dde
abbonno <abbogonluq@gmail.com>
2024-06-02 18:37:08 +0200

    botones de nuevo

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
 require_once('html.php');
-if (isset($_GET['controller'])) {
-  $controller = $_GET['controller'];
-} else {
-  $controller = 'cindex';
-}
 
-echo HTMLrenderWeb();
+$data = getAction($_GET);
+
+echo HTMLrenderWeb($data);
+
+function getAction($p) {
+    $r = [];
+    if (!isset($p['p'])) {
+      $r['controlador'] = 'bienvenida';
+      //$r['metodo']='hello';
+    } else
+    switch ($p['p']) {
+        case 'bienvenida': $r['controlador'] = 'bienvenida';
+                    //$r['metodo'] = 'hello';
+                    break;
+        case 'habitaciones': $r['controlador'] = 'habitaciones';
+                    //$r['metodo'] = 'hello';
+                    break;
+        case 'servicios': $r['controlador'] = 'servicios';
+                    //$r['metodo'] = 'hello';
+                    break;
+        case 'datos': $r['controlador'] = 'datos';
+                    //$r['metodo'] = 'hello';
+                    break;
+        case 'reservas': $r['controlador'] = 'reservas';
+                    //$r['metodo'] = 'hello';
+                    break;
+        default: $r['controlador'] = 'error';
+                //$r['metodo']='hello';
+    }
+    return $r;
+}
 ?>
